90b4f10c6d13bd93f3122785fd68c18906e1f47c
Yuji-Guilherme <guilherme.nagasaki@outlook.com>
2024-03-23 13:31:50 -0300

    solution 560

---
+++ 560. Subarray Sum Equals K/index.js
@@ -0,0 +1,17 @@
+var subarraySum = function(nums, k) {
+    let res = 0;
+    let currSum = 0;
+    let prefixCount = new Map();
+    prefixCount[0] = 1;
+
+    for(var i = 0; i < nums.length; i++) {
+        currSum += nums[i];
+        let diff = currSum - k;
+
+        res += prefixCount[diff] || 0;
+
+        prefixCount[currSum] = 1 + (prefixCount[currSum] || 0);       
+    }
+
+    return res;
+};
