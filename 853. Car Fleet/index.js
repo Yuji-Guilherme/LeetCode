39b3fa4a66ea5d6d14a176db8c82b177089a1c14
Yuji-Guilherme <guilherme.nagasaki@outlook.com>
2024-04-08 19:55:37 -0300

    solution 853 refac

--- 853. Car Fleet/index.js
+++ 853. Car Fleet/index.js
@@ -4,16 +4,16 @@ var carFleet = function(target, position, speed) {
     let res = 0;
     let lastTime = -1;
 
-    for(let i = 0; i < len; i++){
+    for (let i = 0; i < len; i++) {
         map.set(position[i], speed[i]);
     }
 
     const sortedPos = [...map.keys()].sort((a, b) => b - a);
     
-    for(let i = 0; i < len; i++){
+    for (let i = 0; i < len; i++) {
         let time = (target - sortedPos[i]) / map.get(sortedPos[i]);
 
-        if(time > lastTime){ 
+        if (time > lastTime) { 
             res++;
             lastTime = time;
         }
