64ec6fc65fe1e91091ef21a31d070741794050ce
Yuji-Guilherme <guilherme.nagasaki@outlook.com>
2024-04-16 16:57:19 -0300

    solution 21

---
+++ 21. Merge Two Sorted Lists/index.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        var res = new ListNode();
+        var tail = res;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        if (list1 != null) {
+            tail.next = list1;
+        } else if (list2 != null) {
+            tail.next = list2;
+        }
+
+        return res.next;
+    }
+}
